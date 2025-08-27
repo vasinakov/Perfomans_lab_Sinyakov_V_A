1a1c6b6f1e6db5b23d1551cc5f96cad3e927348f
vasinakov <vasinakov1@gmail.com>
2025-08-28 01:46:39 +0300

    Update task1.py

--- task1/task1.py
+++ task1/task1.py
@@ -1,1 +1,14 @@
+import sys
+def f(n, m):
+    mas = [1]
+    res = "1"
+    while True:
+        mas.append((mas[-1] + m - 1) % n)
+        if (mas[-1] == 1):
+            break
+        res += str(mas[-1])
+    res = res.replace('0', str(n))
+    return res
 
+
+print(f(int(sys.argv[1]),int(sys.argv[2])) + f(int(sys.argv[3]), int(sys.argv[4])))
